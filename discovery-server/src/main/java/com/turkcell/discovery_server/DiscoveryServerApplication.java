fb211f2b70ceac2837e56ec863d29f965a86f878
yusufkaan345 <yusufdemirbas345@gmail.com>
2025-10-19 11:05:34 +0300

    enable eureka server  notation

--- discovery-server/src/main/java/com/turkcell/discovery_server/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/turkcell/discovery_server/DiscoveryServerApplication.java
@@ -2,8 +2,9 @@ package com.turkcell.discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
